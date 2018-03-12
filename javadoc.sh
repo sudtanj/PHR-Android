d8f0929bae5f5c2df16cdf95c43431d7c763b96c
Sudono Tanjung <sudtanj@users.noreply.github.com>
2018-03-12 21:33:43 +0700

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
-git checkout gh-pages
-git checkout master -- javadoc
-rsync -r --ignore-existing ./javadoc/* .       
-rm -rf javadoc
-git add -u *
-git commit -m "updating javadoc"
-git push --all
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git config remote.origin.url https://$GITHUB_TOKEN@github.com/sudtanj/PHR-Android.git
+}
+
+commit_website_files() {
+  git checkout gh-pages
+  git checkout master -- javadoc
+  rsync -r --ignore-existing ./javadoc/* .       
+  rm -rf javadoc
+  git add -u *
+  git commit --message "Javadoc by Travis. build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+   git push origin HEAD:gh-pages
+}
+
+setup_git
+commit_website_files
+upload_files
